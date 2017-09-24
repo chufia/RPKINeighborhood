26afc652f6ec7827b8bdd6ce461ba4ce345be7d4
jexus <jexus@localhost.localdomain>
2017-09-24 10:02:10 -0300

    Changes to be committed: 	modified:   Module2/etapa2.py 	modified:   Module2/roas_export.csv  Untracked files: 	Module1/module_out.json 	Module2/module_out.json 	Module2/module_salida.json 	Module2/sample_input.txt 	Module2/sample_output.csv
    translated to english and finished some overlapping details

--- Module2/etapa2.py
+++ Module2/etapa2.py
@@ -7,47 +7,70 @@ Created on Sat Sep 23 16:20:09 2017
 """
 
 import json
-import sys
 import radix
 import csv
 from netaddr import IPSet
 
-rt_prefijos = radix.Radix()
+rt_prefixes = radix.Radix()
+output=[] #lista de diccionarios
 
+
+#load the roas list
 with open('roas_export.csv', 'rb') as csvfile:
     aslines = csv.reader(csvfile, delimiter=',')
     for row in aslines:
-        rt_prefijos.add(row[1])
+        #we just load the prefixes variable into the radix tree
+        rt_prefixes.add(row[1])
 
 
-
-with open("module_out.json", "r") as f: #genera el archivo de salida .json
+#load the json exported from module 1
+with open("module_out.json", "r") as f:
     listas = json.load(f)
-    
+
+
+#we run the loop for EACH AS
 for origin_as_data in listas:
     denom = 0.0
     coverage = 0.0
-    for prefix in origin_as_data["prefixes"]:
-        bgp_pref_len= int(prefix.split("/")[1])
-        #TODO IMPLEMENTAR
-        covering_prefixes = rt_prefijos.search_covering(prefix)
+    
+    bgp_prefixes_comp = IPSet(origin_as_data["prefixes"]) 
+    
+    for prefix in bgp_prefixes_comp.iter_cidrs():
+        prefix_str = str(prefix)
+        
+        covering_prefixes = rt_prefixes.search_covering(prefix_str)
         if len(covering_prefixes) > 0:
-            coverage += pow(2,32-bgp_pref_len)
-            denom += pow(2,32-bgp_pref_len)
+            #if covering_prefixes > 0 then everything is covered and the fraction
+            #is 1/1 then we add all the ips both to the denom and the num
+            coverage += pow(2,32-prefix.prefixlen)
+            denom += pow(2,32-prefix.prefixlen)
             continue
-        covered_prefixes  = rt_prefijos.search_covered(prefix)
+        covered_prefixes  = rt_prefixes.search_covered(prefix_str)
         if len(covered_prefixes) == 0:
+            #if covered_prefixes == 0 then no ip is covered 
+            #then the fraction to add is 0/1 and we only add to the denom
             coverage += 0
+            denom += pow(2,32-prefix.prefixlen)
         else:
+            #otherwise the num is neither 0 nor 1 so we must calculate
             covered_prefixes_str = []
+            
+            #here we compact the nodes so as to avoid overlapping adresses
             for node in covered_prefixes:
                 covered_prefixes_str.append(node.prefix)
             covered_prefixes_comp = IPSet(covered_prefixes_str) 
-            #coverage=0
+            
+            #now we calculate the coverage for every prefix and accumulate
             for covered in covered_prefixes_comp.iter_cidrs():
-                coverage+=pow((2),-(covered.prefixlen-bgp_pref_len))*pow(2,32-bgp_pref_len)
-                denom += pow(2,32-bgp_pref_len)
-    coverage = coverage/denom        
-    print coverage*100
-#with open("module_out.json", "w") as f: #genera el archivo de salida .json
-   # json.dump(listas,f)
+                coverage+=pow((2),-(covered.prefixlen-prefix.prefixlen))*pow(2,32-prefix.prefixlen)
+                denom += pow(2,32-prefix.prefixlen)
+    #here we write coverage as a percentage for the json file
+    coverage = 100*(coverage/denom)        
+    
+       
+    output.append({ # here we create the json dictionary
+        'id':origin_as_data["Origin-AS"],
+        'rpki':coverage
+        })
+with open("module_salida.json", "w") as f: #here we dump the file
+    json.dump(output,f)
